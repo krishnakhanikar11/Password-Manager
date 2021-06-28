1fcbfc6eb4dc9f8c2b6ffad4225b0c7868e97105
Krishna Khanikar <81968471+krishnakhanikar11@users.noreply.github.com>
2021-06-29 01:25:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 
 def pass_gen():
@@ -38,24 +34,52 @@ def pass_gen():
     pass_entry.insert(0,password)
     pyperclip.copy(password)
 
+def file_search():
+    website_name = website_entry.get()
+
+    with open("password-manager.json") as final:
+        data = json.load(final)
+        if website_name in data:
+            email_data = data[website_name]["email"]
+            pass_data = data[website_name]["password"]
+            messagebox.showinfo(title=website_name,message=f"Email: {email_data}\n Password: {pass_data}")
+        else:
+            messagebox.showinfo(title=website_name, message="No info found")
+
 
 
 def file_create():
     website_name = website_entry.get()
     email_use = email_entry.get()
     pass_created= pass_entry.get()
+    new_data = {
+        website_name: {
+            "email" : email_use,
+            "password" : pass_created
+        }
+    }
 
     if len(website_name) == 0 or len(pass_created)==0:
         messagebox.showinfo(title="Empty Field", message="Don't leave and field empty")
 
     else:
-        is_ok = messagebox.askokcancel(title=website_name,
-                                   message=f"These are the details you entered: \nEmail: {email_use} \n"
-                                           f"Website: {website_name} \nPassword: {pass_created}")
+        try:
+            with open("password-manager.json", 'r') as final:
+                data = json.load(final)
 
-        if is_ok:
-            with open(f"./password-manager.txt", mode='a') as final:
-                x = final.write(f"{website_name} | {email_use} | {pass_created}\n")
+        except FileNotFoundError:
+            #will be executed if try block is no executed
+            with open("password-manager.json", 'w') as final:
+                json.dump(new_data, final, indent=4)
+
+        else:
+            #will only be executed if try block is executed
+            data.update(new_data)
+            with open("password-manager.json", 'w') as final:
+                json.dump(data, final, indent=4)
+
+        finally:
+            #will be executed no matter there is error or not
             website_entry.delete(0, END)
             pass_entry.delete(0, END)
 
@@ -73,9 +97,9 @@ canvas.grid(row = 1, column = 1)
 website_label = Label(text="Website",font= ("Courier", 15))
 website_label.grid(row = 2, column = 0)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row = 2, column = 1, columnspan = 2)
+website_entry.grid(row = 2, column = 1)
 
 email_label = Label(text="Email/Username",font= ("Courier", 15))
 email_label.grid(row = 3, column = 0)
@@ -96,5 +120,8 @@ start_button.grid(row = 4, column = 2)
 
 add_button = Button(text="add",width=36, command=file_create)
 add_button.grid(row = 5, column = 1,columnspan=2)
+
+search_button = Button(text="Search",width= 13, command=file_search)
+search_button.grid(row = 2, column = 2)
 
 screen.mainloop()
